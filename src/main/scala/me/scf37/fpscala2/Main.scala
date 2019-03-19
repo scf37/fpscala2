11d983f474fdf3df872123d5ec5cfbe1fe41ac0a
Scf37 <scf370@gmail.com>
2019-03-19 15:39:55 +0300

    move to IOApp

--- src/main/scala/me/scf37/fpscala2/Main.scala
+++ src/main/scala/me/scf37/fpscala2/Main.scala
@@ -1,62 +1,96 @@
 package me.scf37.fpscala2
 
+import cats.Applicative
+import cats.Defer
 import cats.Eval
+import cats.data.EitherT
+import cats.effect.ExitCode
 import cats.effect.IO
+import cats.effect.IOApp
+import cats.effect.Sync
+import cats.implicits._
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import me.scf37.config3.Config3
+import me.scf37.config3.Config3.PrintedConfig
+import me.scf37.fpscala2.db.sql.SqlEffect
 import me.scf37.fpscala2.module.config.ApplicationConfig
 
-object Main {
-  private val env = Option(System.getProperty("env"))
-    .orElse(Option(System.getenv("env")))
-    .getOrElse("")
+object Main extends IOApp {
 
-  def main(args: Array[String]): Unit = {
-
-    val config: ApplicationConfig = ApplicationConfig.load(loadConfig(args, env))
-
-    import me.scf37.fpscala2.db.sql._
-    val app = new Application[Eval, IO, SqlEffect[IO, ?]](config)
-
-    val server = app.serverModule.server.value
-
-    server()
+  private def env[F[_]: Applicative: Defer]: F[Option[String]] = Defer[F].defer {
+    Option(System.getProperty("env"))
+      .orElse(Option(System.getenv("env"))).pure[F]
   }
 
-  private def loadConfig(args: Array[String], env: String): Config = {
-    val reference = ConfigFactory.parseResources("reference.conf")
+  override def run(args: List[String]): IO[ExitCode] = {
+
+    val startedApp: EitherT[IO, EagerExit, Unit] = for {
+      envOpt <- env[EitherT[IO, EagerExit, ?]]
+
+      configInfo <- loadConfig[IO](args.toArray, envOpt.map(_ + ".conf"))
+      (printedConfig, config) = configInfo
+
+      _ <- EitherT.right(IO(println(printedConfig.toString)))
+
+      appConfig = ApplicationConfig.load(config)
+
+      app = new Application[Eval, IO, SqlEffect[IO, ?]](appConfig)
+      server = app.serverModule.server.value
+      _ <- EitherT.right(IO(server()))
+    } yield (())
+
+    startedApp.value.flatMap {
+
+      case Left(eagerExit) =>
+        IO(println(eagerExit.message)).map(_ => eagerExit.code)
+
+      case Right(_) => IO(ExitCode(0))
+    }
+
+  }
+
+  private case class EagerExit(message: String, code: ExitCode)
+
+  /**
+    * Load configuration from multiple sources or fail with error message or exit code
+    *
+    * @param args command line arguments
+    * @param configFile configuration file (optional)
+    * @tparam F
+    * @return
+    */
+  private def loadConfig[F[_]: Sync](
+    args: Array[String],
+    configFile: Option[String]
+  ): EitherT[F, EagerExit, (PrintedConfig, Config)] = {
+
     def isAppConfigKey(key: String) = key.startsWith("app.")
+    def isPasswordKey(key: String) = key.contains("password")
 
-    if (args.sameElements(Seq("--help"))) {
-      println(Config3.help(reference, isAppConfigKey))
-      System.exit(1)
-    }
+    for {
+      reference <- EitherT.pure[F, EagerExit](ConfigFactory.parseResources("reference.conf"))
 
-    val cmdlineConfig = Config3.parse(args) match {
-      case Left(error) =>
-        println(error)
-        System.exit(2)
-        ???
-      case Right(config) => config
-    }
+      _ <- EitherT.cond[F](!args.sameElements(Seq("--help")), (),
+        EagerExit(Config3.help(reference, isAppConfigKey).toString(), ExitCode(1))
+      )
 
-    val config =
-      cmdlineConfig
+      cmdlineConfig <- EitherT.fromEither[F](
+        Config3.parse(args).left.map(error => EagerExit(error.toString(), ExitCode(2)))
+      )
+
+      config = cmdlineConfig
         .withFallback(ConfigFactory.systemProperties())
         .withFallback(ConfigFactory.systemEnvironment())
-        .withFallback(ConfigFactory.load(s"$env.conf"))
+        .withFallback(configFile.fold(ConfigFactory.empty)(ConfigFactory.load))
         .withFallback(reference)
 
-    val errors = Config3.validate(reference, config, isAppConfigKey)
-    if (errors.nonEmpty) {
-      errors.foreach(println)
-      System.exit(2)
-    }
+      errors = Config3.validate(reference, config, isAppConfigKey)
 
-    // print resolved configuration
-    println(Config3.printConfig(reference, config, isAppConfigKey, _.contains("password")))
+      _ <- EitherT.cond[F](errors.isEmpty, (), EagerExit(errors.mkString("\n"), ExitCode(1)))
 
-    config
+      printedConfig = Config3.printConfig(reference, config, isAppConfigKey, isPasswordKey)
+
+    } yield printedConfig -> config
   }
 }
